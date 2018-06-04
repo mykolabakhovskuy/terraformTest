f76329ded251049027a9fa4155aea31ff5e787d3
Mykola Bakhovskiy <mykola_bakhovskiy@epam.com>
2018-06-04 14:31:50 +0300

    config outputs

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -26,10 +26,3 @@ resource "google_compute_instance" "default" {
 
 }
 
-output "zones" {
-    value = ["${google_compute_instance.default.*.zone}"]
-}
-
-output "names" {
-    value = ["${google_compute_instance.default.*.self_link}"]
-}

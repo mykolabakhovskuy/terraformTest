f349ff094ca0d8b41821485127b79c182b8dba66
Mykola Bakhovskiy <Mykola_Bakhovskiy@epam.com>
2018-06-05 10:52:38 +0300

    modified mysql script

--- tr/modules/scripts/mysql.sh
+++ tr/modules/scripts/mysql.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 # Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
-echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password password bahovskuy" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password bahovskuy" | sudo debconf-set-selections
 sudo apt-get -y install mysql-server
-
-
-# Run the MySQL Secure Installation wizard
-#mysql_secure_installation
-
-#sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
-#mysql -uroot -p -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
-
 service mysql restart

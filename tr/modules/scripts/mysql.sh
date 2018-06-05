11faafec760b67f35db3f17683d956095077ad3c
Mykola Bakhovskiy <Mykola_Bakhovskiy@epam.com>
2018-06-05 11:17:44 +0300

    change password to mysql

--- tr/modules/scripts/mysql.sh
+++ tr/modules/scripts/mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "bahovskuy"
 echo "mysql-server mysql-server/root_password password bahovskuy" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password bahovskuy" | sudo debconf-set-selections
 sudo apt-get -y install mysql-server
